e7a02034fbac1adddb091a68886d33db015ef9b9
yug1234coder <yugkarande@gmail.com>
2020-12-09 15:56:49 +0530

    msg

--- Slingshot.js
+++ Slingshot.js
@@ -14,6 +14,23 @@ this.sling3=loadImage('sprites/sling3.png')
         this.sling = Constraint.create(options);
         World.add(world, this.sling);
     }
+attach(body){
+
+this.sling.bodyA=body
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     fly(){
         this.sling.bodyA = null;
--- sketch.js
+++ sketch.js
@@ -75,3 +75,19 @@ function mouseDragged(){
 function mouseReleased(){
     slingshot.fly();
 }
+function keyPressed(){
+
+
+if (keyCode===32){
+
+slingshot.attach(bird.body)
+
+
+}
+
+
+
+
+
+
+}
